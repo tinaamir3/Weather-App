3a6fc5d5e81c3254d9ed0ea7f5d238b88010394d
tinaamir3 <tina.amir3@gmail.com>
2020-12-22 18:36:44 -0500

    changes made to JS, temperature and descriptions added

--- index.js
+++ index.js
@@ -1,52 +1,18 @@
-let now = new Date();
-let h6 = document.querySelector("h6");
-let hours = now.getHours();
-let minutes = now.getMinutes();
-let days = ["Sun", "Mon", "Tue,", "Wed", "Thu", "Fri", "Sat", "Sun"];
-let day = days[now.getDay()];
-h6.innerHTML = `${day} ${hours}:${minutes}`;
+ function displayTemperature(response){
+    let temperatureElement= document.querySelector("#temperature");
+    let cityElement= document.querySelector("#city");
+    let feelsLikeElement= document.querySelector("#feels");
+    let descriptionElement= document.querySelector("#description");
+    let windElement= document.querySelector("#windII");
+    temperatureElement.innerHTML= Math.round(response.data.main.temp);
+cityElement.innerHTML=response.data.name;
+feelsLikeElement.innerHTML=Math.round(response.data.main.feels_like);
+descriptionElement.innerHTML=response.data.weather[0].description;
+windElement.innerHTML=Math.round(response.data.wind.speed);
+console.log (response.data);
+} 
+ let apiKey= "1b8abfcfd13f6be4d6f095c6de05ba7f";
+ let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=${apiKey}&units=metric`;
 
-//weather api
-function displayWeather(response) {
-  console.log(response.data);
-  document.querySelector("#city").innerHTML = response.data.name;
-  let temperature = Math.round(response.data.main.temp);
-  let message = `${temperature}°`;
-  document.querySelector("h3").innerHTML = message;
-  document.querySelector("#feelsLike").innerHTML = Math.round(
-    response.data.main.feels_like
-  );
-  document.querySelector("#presc").innerHTML = response.data.main.humidity;
-  document.querySelector("#windII").innerHTML = response.data.wind.speed;
-}
-function search(event) {
-  event.preventDefault();
-  let apiKey = "1b8abfcfd13f6be4d6f095c6de05ba7f";
-  let units = "metric";
-  let city = document.querySelector("#city-input").value;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(displayWeather);
-}
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", search);
 
-//celsius to fahrenheit vise vs
-function celsiusChange(event) {
-  event.preventDefault();
-  let h3 = document.querySelector("h3");
-  h3.innerHTML = "28°";
-  let p = document.querySelector("p");
-  p.innerHTML = "Feels like: 32°";
-}
-let celsiusLink = document.querySelector("#celsius");
-celsiusLink.addEventListener("click", celsiusChange);
-
-function fahrenheitChange(event) {
-  event.preventDefault();
-  let h3 = document.querySelector("h3");
-  h3.innerHTML = "82°";
-  let p = document.querySelector("p");
-  p.innerHTML = "Feels like: 90°";
-}
-let fahrenheitLink = document.querySelector("#fahrenheit");
-fahrenheitLink.addEventListener("click", fahrenheitChange);
+ axios.get(apiUrl).then(displayTemperature);
