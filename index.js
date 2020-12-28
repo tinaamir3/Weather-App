38b17413cb5bd00dcff09db079ea221114e4a060
tinaamir3 <tina.amir3@gmail.com>
2020-12-27 22:04:27 -0500

    Search Engine functioning

--- index.js
+++ index.js
@@ -47,16 +47,18 @@ iconElement.setAttribute(
     );
     iconElement.setAttribute("alt", response.data.weather[0].description);
 } 
-function search(event){
+function search(city) {
+ let apiKey= "1b8abfcfd13f6be4d6f095c6de05ba7f";
+ let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);  
+}
+function handleSubmit(event){
     event.preventDefault();
     let cityInputElement= document.querySelector("#city-input");
+search(cityInputElement.value);
 }
- let apiKey= "1b8abfcfd13f6be4d6f095c6de05ba7f";
- let city= "Toronto";
- let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
- axios.get(apiUrl).then(displayTemperature);
+search("Sydney");
 
 let form= document.querySelector("#search-form");
-form.addEventListener("submit",search);
+form.addEventListener("submit", handleSubmit);
 
